84bf635a2b8e0dafbfa0128d24614ff7bf3718cf
Swarup Das <78835981+swarupcs@users.noreply.github.com>
2025-09-23 19:33:57 +0530

    setup the tanstack-query

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,21 @@
-import { useState } from 'react'
-
+import { QueryClient, QueryClientProvider } from 'node_modules/@tanstack/react-query/build/legacy';
 import './App.css'
 import AppRoutes from './AppRoutes'
+import { Toaster } from 'sonner';
 
 function App() {
+
+   const queryClient = new QueryClient();
 
 
   return (
     <>
-      <AppRoutes />
+      <QueryClientProvider client={queryClient}>
+        <AppRoutes />
+        <Toaster position='top-center' richColors />
+      </QueryClientProvider>
     </>
-  )
+  );
 }
 
 export default App
